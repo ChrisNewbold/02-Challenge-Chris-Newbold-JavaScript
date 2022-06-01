8b04c3ba8609037771f0cc683a358d9d81395924
Chris Newbold <104353732+ChrisNewbold@users.noreply.github.com>
2022-06-01 23:12:26 +1000

    final commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,14 @@
 // Assignment Code
-const characterAmountRange = document.getElementById('characterAmountRange')
-const characterAmountNumber = document.getElementById('characterAmountNumber')
-const includeLowercaseElement = document.getElementById('includeLowercase')
-const includeUppercaseElement = document.getElementById('includeUppercase')
-const includeNumbersElement = document.getElementById('includeNumbers')
-const includeSymbolsElement = document.getElementById('includeSymbols')
-const form = document.getElementById('passwordGeneratorForm')
-const passwordDisplay = document.getElementById('passwordDisplay')
-// this dictates the lowercase, uppercase, numbers and symbols called
+// I decided to go with const instead of var for this exercise to help keep the code as clean as possible as there should be no need for the identifier to be reassigned.
+const characterAmountRange = document.getElementById("characterAmountRange")
+const characterAmountNumber = document.getElementById("characterAmountNumber")
+const includeLowercaseElement = document.getElementById("includeLowercase")
+const includeUppercaseElement = document.getElementById("includeUppercase")
+const includeNumbersElement = document.getElementById("includeNumbers")
+const includeSymbolsElement = document.getElementById("includeSymbols")
+const form = document.getElementById("passwordGeneratorForm")
+const passwordDisplay = document.getElementById("passwordDisplay")
+// this sets up the list of character codes dictating the lowercase, uppercase, numbers and symbols called using an = array call from low to high (used the ASCII table & Cheat sheet to assist)
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
@@ -18,42 +19,44 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
 ).concat(
   arrayFromLowToHigh(123, 126)
 )
-// this dictates the main trigger to begin the password generator
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
-// this is what generates the password submition
-form.addEventListener('submit', function (e) {
-  e.preventDefault()
-  // This section creates a pop up prompting the user to select a tick box
+// event listener that triggers everytime the input amount changes and syncs the slider and the input section
+characterAmountNumber.addEventListener("input", syncCharacterAmount)
+characterAmountRange.addEventListener("input", syncCharacterAmount)
+// this event listener triggers everytime we submit the form and will prevent the form from submitting and resetting the page
+form.addEventListener("submit", function (e) {
+  e.preventDefault()//this prevents any charCodes from being auto called
+  // This "if" section creates a pop up prompting the user to select a tick box. 
   if (!form.one.checked && !form.two.checked
     && !form.three.checked && !form.four.checked) {
-    alert('at least one box must be checked')
+    alert("at least one box must be checked")
   }
+  // this tells us if its true or false if button is checked and speaks to the character amount passing the value
   const characterAmount = characterAmountNumber.value
   const includeLowercase = includeLowercaseElement.checked
   const includeUppercase = includeUppercaseElement.checked
   const includeNumbers = includeNumbersElement.checked
   const includeSymbols = includeSymbolsElement.checked
+  // tells the script to generate a password calling the variables and values speaking to the selecter elements 
   const password = generatePassword(characterAmount, includeLowercase, includeUppercase, includeNumbers, includeSymbols)
   passwordDisplay.innerText = password
 })
-// this is the over all list of all our character codes that is then looped
+// this uses charCodes to return our unicode and tells it what to include "if". So essentially tells it what to generate the password from
 function generatePassword(characterAmount, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
-  let charCodes = []
-  if (includeLowercase) charCodes = charCodes.concat(LOWERCASE_CHAR_CODES), includeLowercaseElement.checked = false
+  let charCodes = [] // this sets our charCode default to zero
+  if (includeLowercase) charCodes = charCodes.concat(LOWERCASE_CHAR_CODES), includeLowercaseElement.checked = false //this end section (post "),") calls the pop up value if tick boxes = false
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES), includeUppercaseElement.checked = false
   if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES), includeSymbolsElement.checked = false
   if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES), includeNumbersElement.checked = false;
   // this creates the loop
-  const passwordCharacters = []
+  const passwordCharacters = [] // this is a empty array that stores all our passwords
   for (let i = 0; i < characterAmount; i++) {
-    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)] // multiplies a random number between zero and character amount and makes sure it's a integer by doing math dot.floor giving us our charCode
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
   // this is dictating what is returned from the array
-  return passwordCharacters.join('')
+  return passwordCharacters.join("")
 }
-
+// this creates a funtion that generates our arrays and loops through all the variables from low to high then returns the array
 function arrayFromLowToHigh(low, high) {
   const array = []
   for (let i = low; i <= high; i++) {
@@ -61,15 +64,15 @@ function arrayFromLowToHigh(low, high) {
   }
   return array
 }
-
+// this funtion links the slider to the character amount input
 function syncCharacterAmount(e) {
   const value = e.target.value
   characterAmountNumber.value = value
   characterAmountRange.value = value
 }
-
-clipboard.addEventListener('click', () => {
-  const textarea = document.createElement('textarea');
+// this is the 'copy password' clipboard script
+clipboard.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
   const password = passwordDisplay.innerText;
 
   if (!password) { return; }
@@ -77,7 +80,7 @@ clipboard.addEventListener('click', () => {
   textarea.value = password;
   document.body.appendChild(textarea);
   textarea.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   textarea.remove();
-  alert('Password copied to clipboard');
+  alert("Password copied to clipboard");
 });
